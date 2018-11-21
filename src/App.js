4f9a0934f29b37dd295c487eca47eec1198a47b0
Mindaugas <mindgs.v@gmail.com>
2018-11-21 18:55:04 +0200

    lesson6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Form from './Form'
 
 const emailRegex = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
 const phoneRegex = RegExp(/^\d{3}\d{3}\d{3}$/);
@@ -88,75 +89,7 @@ class App extends Component {
         const {formErrors} = this.state;
         return (
             <div className="App wrapper">
-                <div className="form-wrapper">
-                    <h2>Create an Account</h2>
-                    <form onSubmit={this.handleSubmit} noValidate>
-
-                        <div className="firstName">
-                            <label htmlFor="firstName">First Name</label>
-                            <input
-                                type="text"
-                                className={formErrors.firstName.length > 0 ? 'error' : 'valid'}
-                                placeholder="First Name"
-                                name="firstName"
-                                onChange={this.handleChange}
-                                noValidate/>
-
-                            {formErrors.firstName.length > 0 && (
-                                <span className="errorMessage">{formErrors.firstName}</span>
-                            ) }
-                        </div>
-
-                        <div className="lastName">
-                            <label htmlFor="lastName">Last Name</label>
-                            <input
-                                type="text"
-                                className={formErrors.lastName.length > 0 ? 'error' : 'valid'}
-                                placeholder="Last Name"
-                                name="lastName"
-                                onChange={this.handleChange}
-                                noValidate/>
-
-                            {formErrors.lastName.length > 0 && (
-                                <span className="errorMessage">{formErrors.lastName}</span>
-                            ) }
-                        </div>
-
-                        <div className="email">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="text"
-                                className={formErrors.email.length > 0 ? 'error' : 'valid'}
-                                placeholder="Email"
-                                name="email"
-                                onChange={this.handleChange}
-                                noValidate/>
-
-                            {formErrors.email.length > 0 && (
-                                <span className="errorMessage">{formErrors.email}</span>
-                            ) }
-                        </div>
-
-                        <div className="phone">
-                            <label htmlFor="phone">Phone Number</label>
-                            <input
-                                type="text"
-                                className={formErrors.phone.length > 0 ? 'error' : 'valid'}
-                                placeholder="Phone Number"
-                                name="phone"
-                                onChange={this.handleChange}
-                                noValidate/>
-
-                            {formErrors.phone.length > 0 && (
-                                <span className="errorMessage">{formErrors.phone}</span>
-                            ) }
-                        </div>
-
-                        <div className="submitForm">
-                            <button type="submit">Submit Form</button>
-                        </div>
-                    </form>
-                </div>
+                <Form/>
             </div>
         );
     }
